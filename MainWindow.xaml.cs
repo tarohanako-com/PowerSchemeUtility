71ae7f71fc54b1289b36b5acb63569662c32b508
tarohanako <tomatobuilder@gmail.com>
2023-07-11 01:39:43 +0900

    設定値で動作を変更できるように対応

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Threading;
 
 namespace PowerSchemeUtility
@@ -15,27 +17,88 @@ namespace PowerSchemeUtility
     public partial class MainWindow : Window
     {
         const string CommandProcessName = "PowerProfCmd.exe";
+
+        System.Threading.Mutex applicationMutex = null;
+        System.Windows.Forms.NotifyIcon notifyIcon;
         DispatcherTimer rotateTimer = null;
+
+        bool backgroundApplicationModeSetting = false;
+        bool showWindowModeSetting = true;
+        bool changeModeAfterExitSetting = false;
 
         /// <summary>
         /// コンストラクタ
         /// </summary>
         public MainWindow()
         {
+            // 多重起動をチェックする
+            bool createdNew = false;
+            this.applicationMutex = new System.Threading.Mutex(true, Assembly.GetExecutingAssembly().ManifestModule.Name, out createdNew);
+
+            // ミューテックスの初期使用権が取得出来なかった場合は、すでに起動中のプロセスが存在するので終了する
+            if (createdNew == false)
+            {
+                System.Windows.MessageBox.Show($"すでに起動中のため、終了します.", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
+
+            // 常駐アプリケーションとして起動するかの設定値を取得する
+            this.backgroundApplicationModeSetting = false;
+            if (this.LoadConfigulationNumeric("RunningAtBackground", 0) == 1)
+            {
+                this.backgroundApplicationModeSetting = true;
+            }
+
+            // 常駐アプリケーションとして起動した際のウィンドウ表示設定の設定値を取得する
+            this.showWindowModeSetting = true;
+            if (this.LoadConfigulationNumeric("ShowWindow", 1) == 0)
+            {
+                this.showWindowModeSetting = false;
+            }
+
+            // モードを変更したら終了するかの設定値を取得する
+            this.changeModeAfterExitSetting = false;
+            if (this.LoadConfigulationNumeric("ChangeModeAfterExit", 0) == 1)
+            {
+                this.changeModeAfterExitSetting = true;
+            }
+
             // PowerProfCmdが正常実行できるか調べる
             try
             {
                 var result = this.ExecuteProcess(Path.Combine(this.ExecuteFilePath, MainWindow.CommandProcessName), string.Empty);
                 if (result.Item1 != 0)
                 {
-                    MessageBox.Show($"PowerProfCmd.exeの結果が異常です.\nランタイムが不足していないか等、システム環境を再確認してください.\n\n{result.Item2}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                    System.Windows.MessageBox.Show($"PowerProfCmd.exeの結果が異常です.\nランタイムが不足していないか等、システム環境を再確認してください.\n\n{result.Item2}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
                     this.Close();
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"PowerProfCmd.exeが実行できません.\n想定外エラーです.\n\n{ex.ToString()}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                System.Windows.MessageBox.Show($"PowerProfCmd.exeが実行できません.\n想定外エラーです.\n\n{ex.ToString()}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
                 this.Close();
+            }
+
+            // タスクバーに登録する
+            if (this.backgroundApplicationModeSetting == true)
+            {
+                this.notifyIcon = new NotifyIcon();
+                this.notifyIcon.Text = this.Title;
+                this.notifyIcon.Icon = new System.Drawing.Icon(System.Windows.Application.GetResourceStream(new Uri("app_icon.ico", UriKind.Relative)).Stream);
+                this.notifyIcon.Visible = true;
+                this.notifyIcon.MouseClick += (_, e) =>
+                {
+                    // タスクバーのアイコンを左クリックした際にウィンドウを表示する
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        this.Visibility = Visibility.Visible;
+                    }
+                };
+
+                // 右クリックメニューを登録する
+                var contextMenu = new System.Windows.Forms.ContextMenuStrip();
+                contextMenu.Items.Add("常駐終了", null, (_, __) => { this.ForceExit(); });
+                this.notifyIcon.ContextMenuStrip = contextMenu;
             }
 
             this.Initialized += (_, __) =>
@@ -45,6 +108,12 @@ namespace PowerSchemeUtility
                 this.rotateTimer.Interval = new System.TimeSpan(0, 0, 0, 0, 1000);
                 this.rotateTimer.Tick += this.onRotateTimerTick;
                 this.rotateTimer.Start();
+
+                // 常駐アプリケーションとして起動した際のウィンドウの非表示設定を行う
+                if (this.backgroundApplicationModeSetting == true && this.showWindowModeSetting == false)
+                {
+                    this.Visibility = Visibility.Hidden;
+                }
             };
 
             InitializeComponent();
@@ -70,6 +139,13 @@ namespace PowerSchemeUtility
         {
             Tuple<int, string> result = null;
 
+            // 画面を表示していない場合は、何もしない
+            if (this.Visibility == Visibility.Hidden)
+            {
+                return;
+            }
+
+            // 現在の状態を取得する
             await Task.Run(() =>
             {
                 result = this.ExecuteProcess(Path.Combine(this.ExecuteFilePath, MainWindow.CommandProcessName), "/GetActiveSchemeText");
@@ -78,7 +154,7 @@ namespace PowerSchemeUtility
             // 実行結果が異常な場合は、エラーメッセージを表示して終了する
             if (result.Item1 != 0)
             {
-                MessageBox.Show($"現在の状態を取得することができません.\n\n{result.Item2}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                System.Windows.MessageBox.Show($"現在の状態を取得することができません.\n\n{result.Item2}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
                 this.Close();
             }
 
@@ -117,16 +193,26 @@ namespace PowerSchemeUtility
                     return;
                 }
 
+                // 現在のモードを非表示にする
+                this.textBlockCurrentState.Text = string.Empty;
+
+                // モード変更処理の呼び出し
                 var result = this.ExecuteProcess(Path.Combine(this.ExecuteFilePath, MainWindow.CommandProcessName), $"/SetActiveSchemeId {schemeId}");
                 if (result.Item1 != 0)
                 {
-                    MessageBox.Show($"PowerProfCmd.exeの結果が異常です.\n\n{result.Item2}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                    System.Windows.MessageBox.Show($"PowerProfCmd.exeの結果が異常です.\n\n{result.Item2}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                    this.Close();
+                }
+
+                // モードを変更したら終了する設定の場合は、ウィンドウを閉じる
+                if (this.changeModeAfterExitSetting == true)
+                {
                     this.Close();
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"PowerProfCmd.exeが実行できません.\n想定外エラーです.\n\n{ex.ToString()}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                System.Windows.MessageBox.Show($"PowerProfCmd.exeが実行できません.\n想定外エラーです.\n\n{ex.ToString()}", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
                 this.Close();
             }
         }
@@ -149,9 +235,82 @@ namespace PowerSchemeUtility
         private void onHyperLinkClick(object sender, RoutedEventArgs e)
         {
             var result = ExecuteProcess(Path.Combine(this.ExecuteFilePath, MainWindow.CommandProcessName), string.Empty);
-            MessageBox.Show($"{result.Item2.ToString()}", this.Title, MessageBoxButton.OK, MessageBoxImage.Information);
+            System.Windows.MessageBox.Show($"{result.Item2.ToString()}", this.Title, MessageBoxButton.OK, MessageBoxImage.Information);
 
             return;
+        }
+
+        /// <summary>
+        /// ウィンドウクローズ直前のイベントハンドラ
+        /// </summary>
+        /// <param name="sender">イベント発生元オブジェクト</param>
+        /// <param name="e">イベント内容</param>
+        private void onWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.backgroundApplicationModeSetting == true)
+            {
+                // 常駐アプリケーションとして動作させる場合は、ウィンドウを閉じるだけで終了しない
+                e.Cancel = true;
+                this.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                // 終了処理を行う
+                this.ForceExit();
+            }
+
+            return;
+        }
+
+        /// <summary>
+        /// 終了処理を行います
+        /// </summary>
+        private void ForceExit()
+        {
+            // ミューテックスを開放
+            if (this.applicationMutex != null)
+            {
+                this.applicationMutex.Close();
+                this.applicationMutex.Dispose();
+
+                this.applicationMutex = null;
+            }
+
+            // タスクバーのアイコンを削除
+            if (this.notifyIcon != null)
+            {
+                this.notifyIcon.Dispose();
+
+                this.notifyIcon = null;
+            }
+
+            App.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// 数値で構成される設定値を取得します
+        /// </summary>
+        /// <param name="keyName">設定キー名</param>
+        /// <param name="defaultValue">デフォルト値</param>
+        /// <returns>設定値</returns>
+        private int LoadConfigulationNumeric(string keyName, int defaultValue)
+        {
+            int result = defaultValue;
+
+            try
+            {
+                var config = ConfigurationManager.AppSettings[keyName];
+                if (config != null)
+                {
+                    result = int.Parse(config);
+                }
+            }
+            catch
+            {
+                result = defaultValue;
+            }
+
+            return result;
         }
 
         /// <summary>
